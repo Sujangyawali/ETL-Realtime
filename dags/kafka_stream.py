b1ca41eba1bceb0e2dee8cdc79753579a6f87d2c
Sujan Gyawali <gyasujan@gmail.com>
2025-03-29 14:12:47 +0545

    update: Script to load msg into kafka broker

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -37,9 +37,14 @@ def format_data(res):
 
 def stream_data():
     import json
+    import time
+    from kafka import KafkaProducer
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent= 3))
+    producer = KafkaProducer(bootstrap_servers = ['localhost:9092'], max_block_ms = 5000)
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    
+    #print(json.dumps(res, indent= 3))
 
 # with DAG('user_automation',
 #          default_args = default_args,
