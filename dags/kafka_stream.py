cfab0f57fc2330167ef5e128a0e2bea6478f0b8d
Sujan Gyawali <gyasujan@gmail.com>
2025-04-06 13:03:56 +0545

    New: Added scripts for integration between kafka, spark and cassendra

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -39,10 +39,21 @@ def stream_data():
     import json
     import time
     from kafka import KafkaProducer
+    import logging
     res = get_data()
     res = format_data(res)
-    producer = KafkaProducer(bootstrap_servers = ['broker:9092'], max_block_ms = 5000)
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers = ['broker:29092'], max_block_ms = 5000)
+    curr_time = time.time()
+    while True:
+        if time.time() > curr_time + 60:
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f"An error occured: {e}")
+            continue
     
     #print(json.dumps(res, indent= 3))
 
@@ -56,4 +67,4 @@ with DAG('user_automation',
         python_callable = stream_data
     )
 
-stream_data()
+streaming_task
